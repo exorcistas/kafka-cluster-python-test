7d683dc7a4b42e34ba753419d06ac97e49953f28
exorcistas <exorcistas@pm.me>
2022-05-01 13:41:16 +0300

    still not working properly

--- producer.py
+++ producer.py
@@ -2,15 +2,18 @@ from json import dumps
 from kafka import KafkaProducer
 from loguru import logger
 
+def serializer(message):
+    return json.dumps(message).encode('utf-8')
+
 if __name__ == '__main__':
     # initializing the Kafka producer
     producer = KafkaProducer(
         bootstrap_servers = ['localhost:9092'],
-        value_serializer = lambda x:dumps(x).encode('utf-8')
+        value_serializer = serializer
     )
 
     url = 'https://www.internetwache-polizei-berlin.de/vdb/Fahrraddiebstahl.csv'
-    with open('testdata.csv') as infile:
+    with open('testdata.csv',encoding='utf-8') as infile:
         data = infile.readlines()
     logger.info('Sending data to kafka under topic name `biketheft`')
     for d in data:
--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -3,7 +3,7 @@
 # update and install base components
 sudo apt update -y
 sudo apt install -y software-properties-common
-sudo apt-get install -y python3.10 python3-dev python3-pip python3-venv
+sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 python3 -m pip install --upgrade pip
 python3 --version
 
@@ -11,13 +11,17 @@ python3 --version
 sudo apt install docker docker-compose -y
 sudo systemctl enable docker
 sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo chown $USER /var/run/docker.sock
+sudo usermod -aG docker vagrant
+sudo chown vagrant /var/run/docker.sock
 sudo systemctl start docker
 docker --version
 
 # prepare environment
-chown $USER /vagrant_data
+su vagrant
+chown vagrant /vagrant_data
 cd /vagrant_data
 docker-compose up -d
-pip3 install -r requirements.txt
+rm -rf venv
+#python3 -m venv venv
+#source venv/bin/activate
+#pip3 install -r requirements.txt
